87b8f19fb6002eb1d4971d8240b36b193a64a054
BadDust80131 <baddust80131@gmail.com>
2024-08-01 19:31:32 -0700

    Excercise 10 Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let num = Number(number)
+    if (num < 0) {
+        return "OOPS"
+    }
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
+    
+    
+    for (let i = 1; i <= num; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return n1
 };
 
 // Do not edit below this line
