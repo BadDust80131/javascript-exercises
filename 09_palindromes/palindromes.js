92794ed887f5694ea1e3ca8b076fe53e9d42821e
BadDust80131 <baddust80131@gmail.com>
2024-08-01 19:14:51 -0700

    Solved Excercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (pali) {
+    let i = pali.length;
+    let st = "";
+    while (i--) {
+        if (pali.charAt(i) != " " && pali.charAt(i) != "," && pali.charAt(i) != "." && pali.charAt(i) != "!" && pali.charAt(i) != "?") {
+            st += pali.charAt(i);
+        }
+    }
+    st = st.toLowerCase()
+    reverse = st.split("").reverse().join("");
+    if (st == reverse) {
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
